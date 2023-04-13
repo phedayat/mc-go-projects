6fbaf9c0dcc1896e61e19cb348c0d3592a9f6e87
phedayat <phedayat@ucsd.edu>
2023-04-12 19:28:09 -0500

    Updated main.go to the latest working version which can access the MPU6050

--- projects/mpu6050-test/main.go
+++ projects/mpu6050-test/main.go
@@ -2,17 +2,26 @@ package main
 
 import (
 	"fmt"
-	"machine"
 	"time"
+	"machine"
 
 	"tinygo.org/x/drivers/mpu6050"
 )
 
 func main(){
-	machine.I2C0.Configure(machine.I2CConfig{})
-	sensor := mpu6050.New(machine.I2C0)
-	sensor.Configure()
+	// First we get the SDA, SCL pins
+	sda := machine.GPIO0
+	scl := machine.GPIO1
 
+	// Then we configure the I2C0 constant
+	machine.I2C0.Configure(machine.I2CConfig{SDA: sda, SCL: scl})
+	
+	// Now we can get a new instance of the MPU6050 device
+	// We pass in the I2C bus (machine.I2C0)
+	sensor := mpu6050.New(machine.I2C0)
+	sensor.Configure() // run .Configure() to prepare the device
+
+	// Checks that a device is connected
 	connected := sensor.Connected()
 	if !connected {
 		fmt.Println("MPU6050 not found")
@@ -21,15 +30,17 @@ func main(){
 	fmt.Println("MPU6050 found")
 
 	for {
-		ax, ay, az := sensor.ReadAcceleration()
-		rx, ry, rz := sensor.ReadRotation()
+		ax, ay, az := sensor.ReadAcceleration() // get accel vector
+		rx, ry, rz := sensor.ReadRotation() // get rotation vector
 
+		// Create the formatted string for printing
 		s := fmt.Sprintf(
-			"Acceleration: (%f, %f, %f), Rotation: (%f, %f, %f)", 
+			"Acceleration: (%d, %d, %d), Rotation: (%d, %d, %d)", 
 			ax, ay, az, rx, ry, rz,
 		)
 		fmt.Println(s)
 
+		// Sleep
 		time.Sleep(2 * time.Second)
 	}
 }
