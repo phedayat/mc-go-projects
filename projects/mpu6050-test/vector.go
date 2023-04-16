53f1d2e09cab86a96f6f67e8cf2df4954b7b10e6
phedayat <phedayat@ucsd.edu>
2023-04-16 01:21:29 -0500

    Added a basic Vector type and some associated methods

--- projects/mpu6050-test/vector.go
+++ projects/mpu6050-test/vector.go
@@ -6,13 +6,16 @@ import (
 )
 
 type Vector struct {
-	X, Y, Z float64
+	X, Y, Z float32
 }
 
 func (v Vector) Length() float64 {
-	vx2 := math.Pow(v.X, 2)
-	vy2 := math.Pow(v.Y, 2)
-	vz2 := math.Pow(v.Z, 2)
+	x := float64(v.X)
+	y := float64(v.Y)
+	z := float64(v.Z)
+	vx2 := math.Pow(x, 2)
+	vy2 := math.Pow(y, 2)
+	vz2 := math.Pow(z, 2)
 	return math.Sqrt(vx2+vy2+vz2)
 }
 
